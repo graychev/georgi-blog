5abdddb00e41d6c2991ffa71f7fdc64a4a5395cc
Georgi Raychev <raychev.georgi@gmail.com>
2022-09-02 23:55:23 +0300

    Removing database keys

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ from flask_gravatar import Gravatar
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "8BYkEfBA6fO6donzbWlSihBsXox7C0sKR6b")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -57,7 +57,7 @@ class Comment(db.Model):
     parent_post = relationship("BlogPost", back_populates="comments")
     text = db.Column(db.String, nullable=False)
 
-db.create_all()
+# db.create_all()
 
 gravatar = Gravatar(app,
                     size=100,
